b1758177f8e00bb1ec3cb534731d3f11392af1cb
marmot0814 <s1357andy871207@gmail.com>
2020-01-14 05:55:01 +0800

    add set command and get command

--- pc/main.c
+++ pc/main.c
@@ -64,13 +64,21 @@ void send_get(int fd) {
 	struct iapp_header_t iapp_header = {
 		.command = GET
 	};
-	uint8_t iapp_payload[] = "Hello World!!";
-	size_t iapp_payload_size = sizeof(iapp_payload);
+	uint8_t iapp_payload[] = "";
+	size_t iapp_payload_size = 0;
 	uint8_t recv_payload[512];
 	size_t recv_payload_size = iapp(fd, &iapp_header, iapp_payload, iapp_payload_size, recv_payload);
-
-	printf("recv_payload_size: %lu\n", recv_payload_size);
-	printf("recv_payload: %s\n", (char*)recv_payload);
+	dump("recv payload", recv_payload, 0, recv_payload_size);
+}
+void send_set(int fd, uint32_t value) {
+	struct iapp_header_t iapp_header = {
+		.command = SET
+	};
+	uint32_t iapp_payload = value;
+	size_t iapp_payload_size = sizeof(iapp_payload);
+	uint8_t recv_payload[512];
+	size_t recv_payload_size = iapp(fd, &iapp_header, (uint8_t*)(&iapp_payload), iapp_payload_size, recv_payload);
+	dump("recv payload", recv_payload, 0, recv_payload_size);
 }
 void help() {
     printf("\thelp\t\t\tPrint this message\n");
@@ -78,6 +86,9 @@ void help() {
     printf("\tupload <filename>\tUpload binary file to mcu\n");
     printf("\trun\t\t\tStart the code on the mcu\n");
     printf("\tget\t\t\tGet code information on the mcu\n");
+    printf("\tset\t\t\tSet execution options on reset\n");
+    printf("\t\t\t\t0: run the program after reset immediately\n");
+    printf("\t\t\t\t1: run the program after the \"run\" command\n");
 }
 int main(int argc, char **argv) {
 	int fd = connect(argc, argv);
@@ -99,6 +110,11 @@ int main(int argc, char **argv) {
             send_run(fd);
         } else if (!strncmp(ptr, "get", 3)) {
             send_get(fd);
+        } else if (!strncmp(ptr, "set", 3)) {
+            ptr = strtok(NULL, " \n");
+            uint32_t value;
+            sscanf(ptr, "%u", &value);
+            send_set(fd, value);
 		} else if (!strncmp(ptr, "q", 1)) {
 			exit(0);
 		} else goto end;
